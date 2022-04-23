2560c4c23404caa6a2281e79d43b977f57a3f377
securifyapp <104233896+securifyapp@users.noreply.github.com>
2022-04-23 09:21:05 +0530

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "version.json": "6dae183c67ed85369fd0ff4ff7637adc",
 "index.html": "54485fddf721ccd91d3891062053d0bd",
 "/": "54485fddf721ccd91d3891062053d0bd",
-"main.dart.js": "924368a8129ae35568f777bca402391a",
+"main.dart.js": "b15da5bafc31de11ca4f181ffdb3d725",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
